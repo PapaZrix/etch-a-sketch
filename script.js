518acdf4d88b0de5f185f39b39bbfea047132a16
PapaZrix <zrilic313@gmail.com>
2022-07-21 11:52:20 +0200

    Add grid layout to grid container

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const maxCells = 256;
 function makeCells() {
     for (let i = 0; i < maxCells; i++) {
         let cells = document.createElement("div");
-        cells.innerHTML = "TEXT";
+        cells.classList.add("cell");
+        cells.textContent = "TEXT";
         container.appendChild(cells);
     }
 }
