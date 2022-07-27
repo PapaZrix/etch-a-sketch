3ca88155ad1856cd5c645aab1f8367c67bd07139
PapaZrix <zrilic313@gmail.com>
2022-07-27 11:45:52 +0200

    Fix spacing issues with grid items expanding out of defined container width/height, removed the grid items width/height to make it auto-adjust as the grid changes size

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const container = document.querySelector("#container");
 const button = document.querySelector("button");
-let maxCells = 256;
-let allCells = makeCells();
+//let maxCells = 256;
 let grid = 16;
-container.style.gridTemplateColumns = `repeat(${grid}, 1fr`;
+//container.style.gridTemplateColumns = `repeat(${grid}, 1fr`;
 //container.style.gridTemplateRows = `repeat(${grid}, 1fr`;
 
+makeCells();
 
 function makeCells() {
-    for (let i = 0; i < maxCells; i++) {
+    for (let i = 0; i < (grid ** 2); i++) {
         cells = document.createElement("div");
         cells.classList.add("cell");
         container.appendChild(cells);
+        container.style.gridTemplateColumns = `repeat(${grid}, 1fr`;
+        container.style.gridTemplateRows = `repeat(${grid}, 1fr`;
     }
 }
 
@@ -20,13 +22,19 @@ function hoverEffect(e) {
     hover.classList.add("hover");
 }
 
+function pixelSize() {
+    let gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.remove());
+    console.log(gridPixels);
+    makeCells();
+}
+
 container.addEventListener("mouseover", hoverEffect);
 
 button.addEventListener("click", function(e) {
     grid = parseInt(prompt("Enter a number of squares: "));
+    if (grid > 100) return;
     container.style.gridTemplateColumns = `repeat(${grid}, 1fr`;
     container.style.gridTemplateRows = `repeat(${grid}, 1fr`;
-    maxCells = grid * grid;
-    console.log(maxCells);
-    makeCells();
+    pixelSize();
 });
