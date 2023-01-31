2fd6a8ebeb56858b44688596b6e651e3287f7e9d
PapaZrix <zrilic313@gmail.com>
2023-01-31 19:00:14 +0100

    Change entire project layout and add a range input instead of prompting user for the grid size, add options to toggle grid lines and clear the grid

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const container = document.querySelector("#container");
-const button = document.querySelector("button");
-//let maxCells = 256;
+const sizeSlider = document.querySelector(".range");
+const size = document.querySelector(".num");
+const toggleLinesBtn = document.querySelector(".toggle")
+const clearBtn = document.querySelector(".clear")
 let grid = 16;
-//container.style.gridTemplateColumns = `repeat(${grid}, 1fr`;
-//container.style.gridTemplateRows = `repeat(${grid}, 1fr`;
+size.textContent = `Grid size: ${grid} x ${grid}`
+let drawing = false
 
 makeCells();
+
+container.addEventListener("click", draw)
+
+sizeSlider.addEventListener("mousemove", (e) => {
+    updateOnSlide(e.target.value)
+})
+
+sizeSlider.addEventListener("mouseup", (e) => {
+    grid = e.target.value
+    resetGrid()
+})
+
+toggleLinesBtn.addEventListener("click", toggleGridLines)
+
+clearBtn.addEventListener("click", clearGrid)
 
 function makeCells() {
     for (let i = 0; i < (grid ** 2); i++) {
@@ -17,24 +34,50 @@ function makeCells() {
     }
 }
 
-function hoverEffect(e) {
+function paint(e) {
     const hover = e.target;
     hover.classList.add("hover");
 }
 
-function pixelSize() {
-    let gridPixels = container.querySelectorAll('div');
-    gridPixels.forEach(gridPixel => gridPixel.remove());
-    console.log(gridPixels);
+function remove(e) {
+    const hover = e.target
+    hover.classList.remove("hover")
+}
+
+function resetGrid() {
+    let cells = container.querySelectorAll('div');
+    cells.forEach(cell => cell.remove());
     makeCells();
 }
 
-container.addEventListener("mouseover", hoverEffect);
+function draw() {
+    let children = container.childNodes
+    let cells = Array.from(children)
+    if (!drawing) {
+        cells.forEach((cell) => {
+            cell.addEventListener("mouseleave", paint)
+        })
+        drawing = true
+    } else {
+        cells.forEach((cell) => {
+            cell.removeEventListener("mouseleave", paint)
+        })
+        drawing = false
+    }
+}
 
-button.addEventListener("click", function(e) {
-    grid = parseInt(prompt("Enter a number of squares: "));
-    if (grid > 100) return;
-    container.style.gridTemplateColumns = `repeat(${grid}, 1fr`;
-    container.style.gridTemplateRows = `repeat(${grid}, 1fr`;
-    pixelSize();
-});
+function updateOnSlide(value) {
+    size.textContent = `Grid size: ${value} x ${value}`
+}
+
+function clearGrid() {
+    let cells = container.querySelectorAll("div")
+    cells.forEach((cell) => cell.classList.remove("hover"))
+}
+
+function toggleGridLines() {
+    const toggleLinesBtn = document.querySelector(".toggle")
+    let cells = container.querySelectorAll("div")
+    cells.forEach((cell) => cell.classList.toggle("cell"))  
+    toggleLinesBtn.classList.toggle("active")  
+}
